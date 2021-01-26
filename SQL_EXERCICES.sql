780c1e27efc0ce738969a5c7e3bc8f90822600cd
FlorianMorel80 <you@example.com>
2021-01-26 16:58:25 +0100

    début partie 8

--- SQL_EXERCICES.sql
+++ SQL_EXERCICES.sql
@@ -141,7 +141,7 @@ ALTER TABLE `frameworks`
 ALTER TABLE `frameworks`
     MODIFY `version` VARCHAR(20);
     -- OU 
-    -- CHANGE `version` `version` VARCHAR(10);
+    -- CHANGE `version` `version` VARCHAR(20);
 
 
 -- TP 
@@ -152,12 +152,22 @@ ALTER TABLE `frameworks`
 --     changer le type de la colonne phoneNumber en VARCHAR
 --     ajouter les colonnes zipCode(VARCHAR) et city(VARCHAR)
 
-USE CODEX;
+USE codex;
 ALTER TABLE `clients` 
-    DROP COLUMN `secondPhoneNumber`,
-    CHANGE `firstPhoneNumber` `phoneNumber` VARCHAR(15),
+    DROP IF EXISTS `secondPhoneNumber`,
+    CHANGE IF EXISTS `firstPhoneNumber` `phoneNumber` VARCHAR(15),
     ADD `zipCode` VARCHAR(7),
     ADD `city` VARCHAR(15);
+
+    -- OU
+    -- ALTER TABLE `clients` 
+    -- DROP COLUMN `secondPhoneNumber`,
+    -- CHANGE `firstPhoneNumber` `phoneNumber` INT;
+
+-- ALTER TABLE `clients`
+    -- MODIFY `phoneNumber` VARCHAR(15),
+    -- ADD `zipCode` VARCHAR(7),
+    -- ADD `city` VARCHAR(15);
 
 -------------------------------------- END OF PARTIE 3 ------------------------------
 
@@ -198,11 +208,11 @@ VALUES
 
 
 INSERT INTO `frameworks` (`framework`, `version`) 
-VALUES 
-    ('Symfony', 'version 2.8'),
-    ('Symfony', 'version 3'),
-    ('Jquery', 'version 1.6'),
-    ('Jquery', 'version 2.10');
+    VALUES 
+        ('Symfony', 'version 2.8'),
+        ('Symfony', 'version 3'),
+        ('Jquery', 'version 1.6'),
+        ('Jquery', 'version 2.10');
         
 -------------------------------------- END OF PARTIE 4 ----------------------------------
 
@@ -239,17 +249,34 @@ VALUES
 
 
 ---------------Partie 6 : Sélection de données - Élargir les possibilités de la clause WHERE
+
+CREATE TABLE `webDevelopment`.`ide` (
+    `id` SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(40) NOT NULL,
+    `version` VARCHAR(10) NOT NULL,
+    `date` DATE NOT NULL,
+    PRIMARY KEY (`id`)
+    )
+    ENGINE=INNODB;
+
+INSERT INTO `webDevelopment`.`ide` (`name`, `version`, `date`) VALUES ('Eclipse', '3.3', '2007-06-01');
+INSERT INTO `webDevelopment`.`ide` (`name`, `version`, `date`) VALUES ('Eclipse', '3.5', '2009-06-01');
+INSERT INTO `webDevelopment`.`ide` (`name`, `version`, `date`) VALUES ('Eclipse', '3.6', '2010-06-01');
+INSERT INTO `webDevelopment`.`ide` (`name`, `version`, `date`) VALUES ('Eclipse', '3.7', '2011-06-01');
+INSERT INTO `webDevelopment`.`ide` (`name`, `version`, `date`) VALUES ('Eclipse', '4.3', '2013-06-13');
+INSERT INTO `webDevelopment`.`ide` (`name`, `version`, `date`) VALUES ('NetBeans', '7', '2011-04-01');
+INSERT INTO `webDevelopment`.`ide` (`name`, `version`, `date`) VALUES ('NetBeans', '8.2', '2016-10-03');
 -- Exercice 1
 -- Dans la table frameworks, afficher toutes les données de la table ayant une version 2.x (x étant un numéro quelconque).
-    SELECT * FROM `frameworks` WHERE `version` >= '2.' OR `version` <= '2.9';
+    SELECT * FROM `frameworks` WHERE `version` LIKE 'version 2.%';
 
 -- Exercice 2
 -- Dans la table frameworks, afficher toutes les lignes ayant pour id 1 et 3.
-    SELECT * FROM `frameworks` WHERE `id` = '1' OR `id` = '3';
+    SELECT * FROM `frameworks` WHERE `id`IN ('1', '3');
 
 -- Exercice 3
 -- Dans la table ide, afficher toutes les lignes ayant une date comprise entre le premier janvier 2010 et le 31 decembre 2011.
-
+    SELECT * FROM `ide` WHERE `date` BETWEEN '2010-01-01' AND '2011-12-31';
 -------------------------------------- END OF PARTIE 6  ----------------------------------
 
 
@@ -257,29 +284,99 @@ VALUES
 -------------------------------- Partie 7 : Suppression et modification de données------------
 -- Exercice 1
 -- Dans la table languages, supprimer toutes les lignes parlant de HTML.
+    DELETE FROM `languages` WHERE `language` = 'html';
 
 -- Exercice 2
 -- Dans la table frameworks, modifier toutes les lignes ayant le framework Symfony par Symfony2.
+    UPDATE `frameworks` SET `framework` = 'Symfony2' WHERE `framework` = 'Symfony' ;
 
 -- Exercice 3
 -- Dans la table languages, modifier la ligne du langage JavaScript version 5 par la version 5.1.
-
+    UPDATE `languages` SET  `version` = 'Version 5.1' WHERE `language` = 'JavaScript' AND `version` =  'Version 5';
 -------------------------------------- END OF PARTIE 7  ----------------------------------
 
 
 
 ----------------------------------- Partie 8 : Les jointures ---------------------------------
+DROP DATABASE `development`;
+-- Création de la base
+CREATE DATABASE IF NOT EXISTS `development`;
+-- On se positione dans la base nouvellement créé
+USE `development`;
+-- Création de la table languages
+CREATE TABLE IF NOT EXISTS `languages` (
+`id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+`name` VARCHAR(40) NOT NULL,
+PRIMARY KEY (`id`)
+)ENGINE=INNODB;
+-- Création de la table frameworks qui contient une clé étrangère
+CREATE TABLE IF NOT EXISTS `frameworks` (
+`id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+`name` VARCHAR(40) NOT NULL,
+`languagesId` INT NOT NULL,
+PRIMARY KEY (`id`)
+)ENGINE=INNODB;
+-- Insertion des données dans la table languages
+INSERT INTO `languages` (`name`) VALUES ('Assembleur');
+INSERT INTO `languages` (`name`) VALUES ('C');
+INSERT INTO `languages` (`name`) VALUES ('C++');
+INSERT INTO `languages` (`name`) VALUES ('Java');
+INSERT INTO `languages` (`name`) VALUES ('HTML');
+INSERT INTO `languages` (`name`) VALUES ('CSS');
+INSERT INTO `languages` (`name`) VALUES ('JavaScript');
+INSERT INTO `languages` (`name`) VALUES ('PHP');
+INSERT INTO `languages` (`name`) VALUES ('C#');
+INSERT INTO `languages` (`name`) VALUES ('VB');
+INSERT INTO `languages` (`name`) VALUES ('Python');
+INSERT INTO `languages` (`name`) VALUES ('Ruby');
+INSERT INTO `languages` (`name`) VALUES ('Swift');
+-- Insertion des données dans la table frameworks
+INSERT INTO `frameworks` (`name`,`languagesId`) VALUES ('Qt',3);
+INSERT INTO `frameworks` (`name`,`languagesId`) VALUES ('SDK Android',4);
+INSERT INTO `frameworks` (`name`,`languagesId`) VALUES ('Bootstrap',6);
+INSERT INTO `frameworks` (`name`,`languagesId`) VALUES ('Foundation',6);
+INSERT INTO `frameworks` (`name`,`languagesId`) VALUES ('KNACSS',6);
+INSERT INTO `frameworks` (`name`,`languagesId`) VALUES ('Avalanche',6);
+INSERT INTO `frameworks` (`name`,`languagesId`) VALUES ('Miligram',6);
+INSERT INTO `frameworks` (`name`,`languagesId`) VALUES ('Skeleton',6);
+INSERT INTO `frameworks` (`name`,`languagesId`) VALUES ('Hoisin',6);
+INSERT INTO `frameworks` (`name`,`languagesId`) VALUES ('Inuit',6);
+INSERT INTO `frameworks` (`name`,`languagesId`) VALUES ('Mimic',6);
+INSERT INTO `frameworks` (`name`,`languagesId`) VALUES ('Ministrap',6);
+INSERT INTO `frameworks` (`name`,`languagesId`) VALUES ('Lotus',6);
+INSERT INTO `frameworks` (`name`,`languagesId`) VALUES ('Jquery',7);
+INSERT INTO `frameworks` (`name`,`languagesId`) VALUES ('Angular',7);
+INSERT INTO `frameworks` (`name`,`languagesId`) VALUES ('ReactJS',7);
+INSERT INTO `frameworks` (`name`,`languagesId`) VALUES ('Vue.js',7);
+INSERT INTO `frameworks` (`name`,`languagesId`) VALUES ('Ember.js',7);
+INSERT INTO `frameworks` (`name`,`languagesId`) VALUES ('Meteor.js',7);
+INSERT INTO `frameworks` (`name`,`languagesId`) VALUES ('Laravel',8);
+INSERT INTO `frameworks` (`name`,`languagesId`) VALUES ('Symfony',8);
+INSERT INTO `frameworks` (`name`,`languagesId`) VALUES ('CodeIgniter',8);
+INSERT INTO `frameworks` (`name`,`languagesId`) VALUES ('Yii',8);
+INSERT INTO `frameworks` (`name`,`languagesId`) VALUES ('Phalcon',8);
+INSERT INTO `frameworks` (`name`,`languagesId`) VALUES ('Lumen',8);
+INSERT INTO `frameworks` (`name`,`languagesId`) VALUES ('Silex',8);
+INSERT INTO `frameworks` (`name`,`languagesId`) VALUES ('Slim',8);
+INSERT INTO `frameworks` (`name`,`languagesId`) VALUES ('.NET',9);
+INSERT INTO `frameworks` (`name`,`languagesId`) VALUES ('.NET',10);
+INSERT INTO `frameworks` (`name`,`languagesId`) VALUES ('Django',11);
+INSERT INTO `frameworks` (`name`,`languagesId`) VALUES ('Ruby On Rails',12);
 
 -- Exercice 1
 -- Afficher tous les frameworks associés à leurs langages. Si un langage n'a pas de framework l'afficher aussi.
-
+SELECT `frameworks`.`name`,`languages`.`name` FROM `languages` LEFT JOIN `frameworks`
+    ON `frameworks`. `languagesId` = `languages`.`id`;
 -- Exercice 2
 -- Afficher tous les frameworks associés à leurs langages. Si un langage n'a pas de framework ne pas l'afficher.
-
+SELECT `frameworks`.`name`, `languages`.`name` FROM `languages` RIGHT JOIN `frameworks`
+    ON `frameworks`.`languagesId` = `languages`.`id`;
 -- Exercice 3
 -- Afficher le nombre de framework qu'a un langage.
+SELECT COUNT `framework` FROM `frameworks` GROUP BY `language`;
 
 -- Exercice 4
 -- Afficher les langages ayant plus de 3 frameworks.
 
 -------------------------------------- END OF PARTIE 8  ----------------------------------
+
